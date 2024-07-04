be123c5343ed81240bee225f363f4187ab6cc94d
srdango1 <christoferrivera32@gmail.com>
2024-07-03 20:54:25 -0400

    cambios
    seleccion, editar y insertar hechos
    aun no se quedan bien los datos que es el unico detalle

--- final_code/src/Formulario.jsx
+++ final_code/src/Formulario.jsx
@@ -6,6 +6,8 @@ const Formulario = () => {
 
     const url = "http://45.236.130.191/tours.php"
     const [lugares, setLugares] = useState([])
+    const [nombre, setNombre] = useState("")
+    const [destino, setDestino] = useState("")
 
     useEffect(() => {
         axios.get(url).then(response => {
@@ -14,28 +16,36 @@ const Formulario = () => {
             })
     }, [])
 
+    const guardar = () => {
+      const lugar = {id: lugares.length + 1,name: nombre, destino: destino}
+      setLugares([...lugares, lugar])
+      localStorage.setItem("lugares", JSON.stringify([...lugares, lugar]))
+    }
+
   return (
     <>
       <div className='container'>
         <div className="row">
           <div className='col-6'>
             <div className='col'>
-              <label htmlFor="name" className='form-label'>
-                Nombre {" "}
+              <label htmlFor="name" className='form-label'> 
+                Nombre 
               </label>
-              <input type="text" id='names' className='form-control'/>
+              <input type="text" id='names' className='form-control' value={nombre} onChange={e => setNombre(e.target.value)}/>
             </div>
             <div className='col'>
-              <label htmlFor="name" className='form-label'>Destino {" "}</label>
-              <input type="text" id='destinos' className='form-control'/>
+              <label htmlFor="name" className='form-label'>
+                Destino 
+              </label>
+              <input type="text" id='destinos' className='form-control' value={destino} onChange={e => setDestino(e.target.value)}/>
             </div>
-            <div className='col'>
-              <button className='btn btn-success'>Guardar</button>
+            <div className='col mt-3'>
+              <button className='btn btn-success' onClick={guardar}>Guardar</button>
             </div>
           </div>
         </div>
         
-        <div className='col-6'>
+        <div className='col-6 '>
             <Ubicaciones lugare={lugares}/>
         </div>
       </div>
--- final_code/src/Ubicaciones.jsx
+++ final_code/src/Ubicaciones.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-const Ubicaciones = ({lugare}) => {
+const Ubicaciones = () => {
+    const [filaseleccionada, setFilaseleccionada] = useState (null);
+    const [lugare, setLugares] = useState([]);
+    
+
+    useEffect(() => {
+        const lugare = JSON.parse(localStorage.getItem("lugares")) || [];
+        setLugares(lugare);
+    }, []);
+
+    const eliminar = (id) => {
+        const lugarelimnado = lugare.filter(lugar => lugar.id !== id)
+        setLugares(lugarelimnado);
+        localStorage.setItem("lugares", JSON.stringify(lugarelimnado));
+        setFilaseleccionada(null);
+    };    
+    
+
+    const seleccionfila = (row) => {
+        setFilaseleccionada(row); 
+      };
   return (
     <>
         <table className='table table-stripped'>
@@ -8,17 +28,35 @@ const Ubicaciones = ({lugare}) => {
                 <tr>
                     <th>Nombre</th>
                     <th>Destino</th>
+                    <th>Accion</th>
                 </tr>
             </thead>
             <tbody>
-                {lugare.map(l => (
-                    <tr>
-                        <td>{l.name}</td>
-                        <td>{l.destino}</td>
+                
+                {lugare && lugare.map((row) => (
+                    <tr key={row.id} onClick={() => seleccionfila(row)}>
+                        <td>{row.name}</td>
+                        <td>{row.destino}</td>
+                        <td>
+                            <button className='btn btn-danger' onClick={() => eliminar(row.id)} >
+                            Eliminar
+                            </button>
+                        </td>
                     </tr>
                 ))}
             </tbody>
         </table>
+        <div>
+        {filaseleccionada ? (
+          <div>
+            <h2>Fila Seleccionada</h2>
+            <p>Nombre: {filaseleccionada.name}</p>
+            <p>Pais: {filaseleccionada.destino}</p>
+
+          </div> ) : (
+            <p>Seleccione una fila haciendo clic en ella.</p>
+          )}
+          </div>
     </>
   )
 }
